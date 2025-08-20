f2107e9b0aba70d308b71b2d4f8c1c7034d75c13
parthpatel095 <139727874+parthpatel095@users.noreply.github.com>
2025-08-20 14:52:45 +0530

    Update userRoute.js

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { register, login } from '../controller/userController.js';
+import { register, login,sendOTP, 
+  verifyOTP, 
+  resendOTP ,
+forgotPassword,
+  resetPassword,
+  validateResetToken} from '../controller/userController.js';
 
 // @route   POST /api/users/register
 // @desc    Register a new user
@@ -13,5 +18,14 @@ router.post('/register', register);
 router.post('/login', login);
 // router.get('/basic-info/:email', getBasicUserInfo);
 
+//OTP
+router.post('/send-otp', sendOTP);
+router.post('/verify-otp', verifyOTP);
+router.post('/resend-otp', resendOTP);
+
+// Password Reset Routes
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
+router.get('/validate-reset-token/:token', validateResetToken);
 
 export default router;
