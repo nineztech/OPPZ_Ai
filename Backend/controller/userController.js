f803c8d0606060a4a5821f42b445fc17a8e0ffe9
parthpatel095 <139727874+parthpatel095@users.noreply.github.com>
2025-09-22 18:45:43 +0530

    Update userController.js

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -39,8 +39,8 @@ const createTransporter = () => {
     
     const transporter = nodemailer.createTransport({
       host: 'smtpout.secureserver.net',
-      port: 465,
-      secure: true, // Use SSL
+      port: 587,
+      secure: false, // Use SSL
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
